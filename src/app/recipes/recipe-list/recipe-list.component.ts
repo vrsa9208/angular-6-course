340a864af05ce49540ef4552d81959fc81135ad7
vrsa9208 <vrsa9208@gmail.com>
2018-06-23 10:48:30 -0500

    Add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'simply a test', 'https://cdn.oem.com.mx/elesto/2018/06/corea-vs-mexico-600x237.jpg')
+  ];
 
   constructor() { }
 
